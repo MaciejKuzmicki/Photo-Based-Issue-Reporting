3e3d2404b02793f0276c65b4791e972534fe52c8
macius02 <mkuzmicki2002@gmail.com>
2024-03-28 18:56:11 +0100

    Started addDefectScreen

--- MobileApp/src/components/Defect.tsx
+++ MobileApp/src/components/Defect.tsx
@@ -13,14 +13,14 @@ const Defect = ({
 }) => {
   return (
     <Card mode="outlined">
-      <View style={{flexDirection: 'row', alignItems: 'center'}}>
-        <View style={{flex: 2}}>
-          <Image style={{resizeMode: 'center'}} source={{uri: imageUrl}} />
+      <View style={styles.view}>
+        <View style={styles.childView}>
+          <Image style={styles.image} source={{uri: imageUrl}} />
         </View>
         <Card.Content style={{flex: 3}}>
           <Text variant="titleLarge">{location}</Text>
           <Text variant="bodyMedium">{description}</Text>
-          <Text variant="bodyMedium">
+          <Text variant="bodyMedium" style={{fontWeight: 'bold'}}>
             {DateFormatting.formatDateTime(date)}
           </Text>
         </Card.Content>
@@ -31,11 +31,21 @@ const Defect = ({
 
 const styles = StyleSheet.create({
   image: {
-    width: 100,
-    height: 100,
-    resizeMode: 'center',
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
     borderRadius: 15,
     marginLeft: 5,
+  },
+  childView: {
+    flex: 2,
+    justifyContent: 'center',
+    maxHeight: 250,
+    maxWidth: 250,
+  },
+  view: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });
 export default Defect;
--- MobileApp/src/screens/AddDefectScreen.tsx
+++ MobileApp/src/screens/AddDefectScreen.tsx
@@ -1,8 +1,94 @@
-import React, {useRef} from 'react';
-import {View, TouchableOpacity, Text} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  TextInput,
+  StyleSheet,
+  Platform,
+  PermissionsAndroid,
+  Alert,
+} from 'react-native';
 import {RNCamera} from 'react-native-camera';
+import CustomButton from '../components/CustomButton.tsx';
+import {AddDefectRequest} from '../DTO/AddDefectRequest.ts';
+import Geolocation from 'react-native-geolocation-service';
 
 const AddDefectScreen = () => {
-  return <View style={{flex: 1}} />;
+  const [description, setDescription] = useState('');
+  const [location, setLocation] = useState('');
+
+  const handleAddDefect = async () => {
+    const defectData: AddDefectRequest = {
+      description,
+      location,
+    };
+    await requestLocationPermission();
+  };
+
+  const requestLocationPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Location required',
+          message: 'We need your location know where the problem is',
+          buttonPositive: 'Allow',
+          buttonNegative: 'Deny',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        fetchLocation();
+      } else {
+        Alert.alert('Error', 'Permission denied');
+      }
+    } catch (error) {
+      Alert.alert('Error', error.message);
+    }
+  };
+
+  const fetchLocation = () => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const {latitude, longitude} = position.coords;
+        const locString = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
+        setLocation(locString); // Store location string
+        Alert.alert('Success', locString);
+      },
+      error => {
+        Alert.alert('Error fetching location', error.message);
+      },
+      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.textInput}
+        multiline
+        placeholder="Enter your text here"
+        onChangeText={setDescription}
+        value={description}
+      />
+      <CustomButton press={handleAddDefect} text="Confirm" type="primary" />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 20,
+  },
+  textInput: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    textAlignVertical: 'top',
+    padding: 10,
+  },
+});
 export default AddDefectScreen;
